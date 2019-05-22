a905c88cb3bc460a01c0101f2c0962ae71df1aee
ttup7777 <31881924+ttup7777@users.noreply.github.com>
2019-05-22 03:19:17 +0200

    Update orders.js

--- handlers/orders.js
+++ handlers/orders.js
@@ -4,6 +4,13 @@ const app = express();
 const {redisClient, getAllIds, config, genId} = require("../data");
 
 
+const ordercol = {
+    userid: "userid",
+    orderid: "orderid",
+    itemid: "itemid",
+    number: "number"
+};
+
 app.post("/create/:usr_id", async function (req, res, next) {
 
     const {usr_id} = req.params;
@@ -43,5 +50,71 @@ app.get("/find/:id", function (req, res, next) {
         res.send(response);
     });
 });
+
+
+app.post("/additem/:id/:item_id", function (req,res,next){
+
+    const {id, item_id} = req.params;
+    number = 1;
+   
+    redisClient.hvals(item_id, (err, value)=>{
+        // if the item exists
+        if (value[number] == null)
+            return next(err);
+         // if the item stock enough
+        if (value[number] <1)
+            return next(err);
+        
+            // console.log(value[number]);
+        redisClient.hincrby(id, item_id , number, (err, count) =>{
+            if (err)
+                return next(err);
+            if (count > value[number])
+                return next(err);
+            
+            res.json({"id": id, "itemid":item_id,"count": count});
+
+         });
+        
+        });
+ 
+});
+
+app.post("/removeitem/:id/:item_id", function (req,res,next){
+
+    const {id, item_id} = req.params;
+    number = 1;
+    
+    redisClient.hvals(item_id, (err, value)=>{
+    // if the item exists
+        if (value[number] == null)
+            return next(err);
+        redisClient.hincrby(id, item_id , -number, (err, count) =>{
+            if (err)
+                return next(err);
+            if (count <0)
+                return next(err);
+            
+            res.json({"id": id, "itemid":item_id,"count": count});
+
+         });
+        
+        })
+ 
+});
+
+
+// app.post("/checkout/:id", function (req,res,next){
+   
+//     // calling payment service
+
+
+//     // subtract the stocks
+
+
+//     // return the status
+// }
+
+// );
 
 module.exports = app;
